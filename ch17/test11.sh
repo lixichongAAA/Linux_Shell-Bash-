ada73250a91ac8604905e0b6f92ef11581ab73ac
Lxc <1104426357@qq.com>
2023-11-09 20:14:47 +0800

    add sh

---
+++ ch17/test11.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# adding values in an array
+
+function addarray {
+    local sum=0
+    local newarray
+    newarray=(`echo "$@"`)
+    for value in ${newarray[*]}
+    do
+        sum=$[ $sum + $value ]
+    done    
+    echo $sum
+}
+
+myarray=(1 2 3 4 5)
+echo "The original array is ${myarray[*]}"
+result=$(addarray ${myarray[*]})
+echo "The result is $result"
